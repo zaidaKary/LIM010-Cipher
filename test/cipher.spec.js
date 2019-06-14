0ddf90ffae3e3521e2f31f532704a1211785d7d0
zaidaKary <zaida.260194@gmail.com>
2019-06-14 10:25:40 -0500

    Cipher actualizado

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -17,6 +17,11 @@ describe('cipher', () => {
     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
       assert.equal( cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg");
     });
+    
+    it('debería retornar "3456789012" para "0123456789" con offset 33', () => {
+      assert.equal( cipher.encode(33,"0123456789"),"3456789012");
+    });
+
     it('debería retornar " " para " " con offset 33', () => {
       assert.equal( cipher.encode(33," ")," ");
     });
@@ -36,6 +41,10 @@ describe('cipher', () => {
       assert.equal( cipher.decode(33,"hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
     });
 
+    it('debería retornar "0123456789" para "3456789012" con offset 33', () => {
+      assert.equal( cipher.decode(33,"3456789012"),"0123456789");
+    });
+    
     it('debería retornar " " para " " con offset 33', () => {
       assert.equal( cipher.decode(33," ")," ");
     });
